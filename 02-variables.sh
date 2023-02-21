6d979d2748b8ebaf53574da81b86f2fd0e0fd6cf
sriharitirumala <sritirumala30@gmail.com>
2023-02-21 13:08:58 +0530

    [Token-6] variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,6 @@
-Date=2023-02-21
-echo "Hello there, Good Morning today date is ${Date}"
+DATE=2023-02-21
+echo "Hello there, Good Morning today date is ${DATE}"
 
 #command substitution
-Date=${date}
+DATE=${date}
 echo "Hello, Good Morning, Today Date is ${DATE}"
