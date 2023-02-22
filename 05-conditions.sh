a358bbab160ccb6af0e8277e16b32f371308be58
sriharitirumala <sritirumala30@gmail.com>
2023-02-22 13:45:25 +0530

    [Token-7] added new items

--- 05-conditions.sh
+++ 05-conditions.sh
@@ -1,10 +1,12 @@
 number=$1
 string=$2
 
-if [ "${number}" -eq 5 ]: then # then can go in next line
+if [ "${number}" -eq 5 ]; then # then can go in next line
   echo number is 5
 fi
 
 if [ "${string}" == abc ]; then
   echo string is abc
+else
+  echo string is not abc
 fi
